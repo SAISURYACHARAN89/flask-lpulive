9cf63d8571a783ad183cf23eb28341f612514e0b
Sai Surya Charan P <psaisuryacharan@gmail.com>
2024-09-08 17:15:16 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,24 @@
-from flask import Flask
+from flask import Flask, jsonify
+import requests
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+@app.route('/get_data', methods=['GET'])
+def get_data():
+    """
+    API endpoint to get user details from LPU Live.
+    """
+    id = "12223854"
+    token = "9daacfb1e97a628660431de6c9442481"
+    url = f"https://lpulive.lpu.in/fugu-api/api/chat/groupChatSearch?en_user_id={token}&search_text={id}&user_role=USER"
+    try:
+        res = requests.get(url, headers={"app_version": "1.0.0", "device_type": "WEB"}).json()
+        users = res.get("data", {}).get("users", [])
+        if not users:
+            return jsonify({"detail": "No user found."}), 404
+        return jsonify({"users": users}), 200
+    except Exception as e:
+        return jsonify({"error": "error-2", "message": str(e)}), 500
 
-@app.route('/about')
-def about():
-    return 'About'
+if __name__ == '__main__':
+    app.run()
