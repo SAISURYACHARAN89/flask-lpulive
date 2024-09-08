20079ac9502ffc40f5261a34c870ba50a6286892
Sai Surya Charan P <psaisuryacharan@gmail.com>
2024-09-08 17:26:13 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -3,7 +3,7 @@ import requests
 
 app = Flask(__name__)
 
-@app.route('/get_data', methods=['GET'])
+@app.route('/', methods=['GET'])
 def get_data():
     """
     API endpoint to get user details from LPU Live.
